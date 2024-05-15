856ef1615ebfdade942c5fb8bf88fc286945d45c
Lukas Emanuel Erdmann <89515571+Lukas-Erdmann@users.noreply.github.com>
2024-05-15 11:09:41 +0200

    Created ConsoleApplication and renamed executable to Main

--- src/main/java/com/example/stlviewer/control/PolyhedronController.java
+++ src/main/java/com/example/stlviewer/control/PolyhedronController.java
@@ -1,5 +1,23 @@
 package com.example.stlviewer.control;
 
+import com.example.stlviewer.model.Polyhedron;
+
 public class PolyhedronController
 {
+    private Polyhedron polyhedron;
+
+    public PolyhedronController (Polyhedron polyhedron)
+    {
+        this.polyhedron = polyhedron;
+    }
+
+    public double calculateVolume ()
+    {
+        return this.polyhedron.calculateVolume();
+    }
+
+    public double calculateSurfaceArea ()
+    {
+        return this.polyhedron.calculateSurfaceArea();
+    }
 }
--- src/main/java/com/example/stlviewer/view/ConsoleApplication.java
+++ src/main/java/com/example/stlviewer/view/ConsoleApplication.java
@@ -1,5 +1,13 @@
 package com.example.stlviewer.view;
 
+import java.util.Scanner;
+
 public class ConsoleApplication
 {
+    private Scanner scanner;
+
+    public ConsoleApplication ()
+    {
+        this.scanner = new Scanner(System.in);
+    }
 }
